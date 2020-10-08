6f54876bfa95d268197c8f26120e8da06c1bcc94
chadmorris55 <71134162+chadmorris55@users.noreply.github.com>
2020-10-08 10:13:21 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * have user input 10 numbers, finds the largest one
+ * @Chad 
  */
 public class Main {
 
@@ -9,7 +10,21 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner for user input
+    Scanner input = new Scanner(System.in);
+    //creates the 10 element array 
+    int [] numbers = new int [10];
+    //asks user to input 10 numbers and records them
+    System.out.println("Please enter in 10 integers to put into the array");
+    for (int i = 0; i < numbers.length; i++){
+      numbers[i] = input.nextInt();
+    }
+    int largest = numbers[0];
+    for (int j = 1; j < numbers.length; j++){
+      if (largest < numbers[j]){
+        largest = numbers [j];
+      }
+    }
+    System.out.println("The largest number is " + largest);
   }
 }
